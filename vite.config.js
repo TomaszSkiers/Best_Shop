575247cb110d1b64435d324cde62aff330ed4405
TomaszSkiers <tomasz.skiers@gmail.com>
2024-07-26 11:23:21 +0200

    dist

--- vite.config.js
+++ vite.config.js
@@ -3,20 +3,18 @@ import { defineConfig } from "vite";
 /**
  * Path to exercise folder
  */
-const exercisePath = ".";
+// const exercisePath = ".";
+const srcPath = ".";
 
 /**
  * Don't change those lines below
  */
 export default defineConfig({
-  root: exercisePath,
+  // root: exercisePath,
+  root: srcPath,
   server: {
     port: 3000,
   },
-  base: "/Best_Shop/" ,// <===== tutaj nazwa naszego repozytorium
-  base: './', // Use relative paths
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-  },
+  base: "/Best_Shop/dist/" // <===== tutaj nazwa naszego repozytorium
+  
 });
